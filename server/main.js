b3d8472001a01085c39a72961a341d534f453772
Shannon H <shannon594@hotmail.com>
2021-05-22 16:12:03 -0500

    fixed the port issue

--- server/main.js
+++ server/main.js
@@ -4,6 +4,6 @@ const app = express(); // create express app
 
 app.use(express.static("./dist/"));
 // start express server on port 5000
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("server started on port 5000");
 });
